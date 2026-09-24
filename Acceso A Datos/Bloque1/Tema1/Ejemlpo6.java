107f628a9390eca216b8e75b16f2cc24422c00c0
Martin Moreno Lopez <mml0080@alu.medac.es>
2026-09-24 12:58:31 +0200

    wfaf

--- Acceso A Datos/Bloque1/Tema1/Ejemlpo6.java
+++ Acceso A Datos/Bloque1/Tema1/Ejemlpo6.java
@@ -1,7 +1,24 @@
+import java.io.RandomAccessFile;
+
 public class Ejemlpo6 {
     public static void main(String[] args) {
         
+        try {
+            
+            RandomAccessFile file = new RandomAccessFile("./texto.txt", "rw");
+            file.seek(5);
+            long filePointer = file.getFilePointer();
+             System.out.println("Puntero ANTES de leer: " + file.getFilePointer()); // te dirá 5
+            int unByte = file.read();
+            System.out.println("Puntero DESPUES de leer: " + file.getFilePointer()); // te dirá 6
+            System.out.println((char) unByte);
+            file.write('X'); //Escribirá la letra F
+            System.out.println("Puntero DESPUES de escribir: " + file.getFilePointer()); // te dirá 7
+            file.close();
         
-
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("error");
+        }
     }
 }
